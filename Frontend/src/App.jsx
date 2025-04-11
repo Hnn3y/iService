4137a902465616babc55596f97131300420a1820
suruaino <studymail4suru@gmail.com>
2025-04-11 18:52:45 +0100

    added known neccessary packages and dependencies

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,32 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import './App.css';
+
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+// import { Button } from '@shadcn/ui';
+// import { Button } from "@shadcn/ui"
+import Button from '@mui/material/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Emma + Henny</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <Button variant="contained" color="primary">
+        AM MUI BTN
+      </Button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+        Enjoy your coffee fontawesome icon! <FontAwesomeIcon icon={faCoffee} />
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <p className="read-the-docs border border-red-300 text-red-800">
+        Fronteend lover checked some stuffs out.
       </p>
     </>
   )
