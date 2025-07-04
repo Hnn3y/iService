c5c8cfe1bbeb9b6fd84ceeb777aa92002e9cc9cd
Enijeshiku Eniola <eenijeshiku@gmail.com>
2025-07-04 18:09:38 +0100

    fix: Mainlayout conflict fixed

--- Frontend/src/layouts/mainlayout/Mainlayout.jsx
+++ Frontend/src/layouts/mainlayout/Mainlayout.jsx
@@ -1,14 +1,14 @@
 
-import { Outlet } from 'react-router-dom';
+import { Outlet, Link } from 'react-router-dom';
+import styles from './mainlayout.module.css';
 import Header from '../../components/header/Header';
 import Footer from '../../components/footer/Footer';
 
 const MainLayout = () => {
-
     return(
-        <div className='flex flex-col min-h-screen bg-gray-50 text-gray-900'>
-            <Header/>
-            <main className='flex-grow w-full max-w-7xl mx-auto px-4 py-6'>
+        <div className={`w-full ${styles.mainlayout}`}>
+            <Header />
+            <main className='w-[100%] h-full flex flex-col items-center  overflow-y-scroll'>
                 <Outlet />
             </main>
             <Footer />
